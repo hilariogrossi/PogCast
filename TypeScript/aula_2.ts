86eb3e49c94bcab52fcd66acc9c3e17f9083ba41
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2024-04-16 09:54:21 -0300

    Atualização 16/04/2024

---
+++ TypeScript/aula_2.ts
@@ -0,0 +1,33 @@
+let a: [string, any] [] = [
+    ['url', 'http://localhost'],
+    ['port', 8080],
+
+];
+
+function funcao_1() {
+    a.forEach(element => {
+        console.log(element[0]);
+
+    });
+
+}
+
+funcao_1();
+
+
+enum CardSuits {Clubs, Hearts, Spades, Diamonds}
+
+function funcao_2() {
+    let b = 0
+
+    if (b === CardSuits.Clubs) {
+        alert('True');
+
+    } else {
+        alert('False');
+
+    }
+
+    console.log(CardSuits.Clubs);
+
+}
